c2906cec73cfe7c9d13440fc6c047f01d354dda4
tomlauwyck <tom.lauwyck@gmail.com>
2012-07-19 20:16:16 +0200

    Upload basic app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EpisodeTracker::Application.config.secret_token = '53dca7ec0f6c854acdacdcddce84fd185f2aef2ad1a9f30662c25d7787906b94efcb0305c4639928a92d60541b2eea18b04fa280bb520cf31cec64f2ab947c89'
