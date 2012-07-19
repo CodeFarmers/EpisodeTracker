5099f71f3dc440159390527f9e92b322fb3b7d1a
tomlauwyck <tom.lauwyck@gmail.com>
2012-07-19 20:56:51 +0200

    Static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 EpisodeTracker::Application.routes.draw do
+
+  get "static_pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ EpisodeTracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  #root :to => ''
 
   # See how all your routes lay out with "rake routes"
 
