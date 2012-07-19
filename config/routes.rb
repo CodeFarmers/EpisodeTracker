c73d47df1b405bf7bb22c71a37f7b7c24fe18b12
tomlauwyck <tom.lauwyck@gmail.com>
2012-07-19 21:57:41 +0200

    Renamed StaticPages => Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 EpisodeTracker::Application.routes.draw do
 
-  get "static_pages/home"
+  get "pages/home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +51,7 @@ EpisodeTracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'static_pages#home'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
