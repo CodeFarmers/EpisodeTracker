28addbae4d9e7ff1c3c15a014001700a7a2fbcdd
tomlauwyck <tom.lauwyck@gmail.com>
2012-11-12 22:40:02 +0100

    Moved some functions around

--- app/helpers/api_connector.rb
+++ app/helpers/api_connector.rb
@@ -2,33 +2,22 @@ class ApiConnector
   require 'net/http'
 
   def initialize
-    @mirror_path = "http://thetvdb.com"
-    @api_key = "4F5EC797A9F93512"
-    @api_url = @mirror_path + "/api/" + @api_key
-
-    self.get_response_from_url("/Updates.php?type=none")
-    ap "Hello"
+    mirror_path = "http://thetvdb.com"
+    api_key = "4F5EC797A9F93512"
+    api_url = mirror_path + "/api/" + api_key
+    url = URI.parse("#{mirror_path}/api/Updates.php?type=none")
+    res = Net::HTTP.get_response(url)
+    ap res.body
+    get_time_from_response(res)
 
   end
 
-
+  def get_time_from_response(response)
+    ap response.body
+  end
 
   def getSeries
-    self.get_response_from_url("/GetSeries.php?seriesname=")
+    #self.get_response_from_url("/GetSeries.php?seriesname=")
   end
 
-
-
-
-  def get_response_from_url(url)
-    url = URI.parse("#{@mirror_path}/api#{url}")
-    ap url
-    res = Net::HTTP.get_response(url)
-    #req = Net::HTTP::Get.new(url.path)
-    #res = Net::HTTP.start(url.host, url.port) {|http|
-    #  ap req
-    #  http.request(req)
-    #}
-    ap res
-  end
 end
