2dd7a419a4efdfa696c5a8a947bec8511d8ec8ed
tomlauwyck <tom.lauwyck@gmail.com>
2013-02-11 21:07:10 +0100

    Added configure link for admins and test for application helper

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe ApplicationHelper do
+  describe "#flash_class" do
+
+    it "returns correct styles for notice" do
+      flash_class(:notice).should == "alert alert-info"
+    end
+
+    it "returns correct styles for success" do
+      flash_class(:success).should == "alert alert-success"
+    end
+
+    it "returns correct styles for error" do
+      flash_class(:error).should == "alert alert-error"
+    end
+
+    it "returns correct styles for alert" do
+      flash_class(:alert).should == "alert alert-error"
+    end
+  end
+end
