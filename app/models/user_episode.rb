46d364709e22c637b5ca28ef6a0518607be35d21
Tom Lauwyck <tom.lauwyck@gmail.com>
2013-11-17 15:43:47 +0100

    user_episodes should be unique on episode_id and user_id

--- app/models/user_episode.rb
+++ app/models/user_episode.rb
@@ -1,5 +1,6 @@
 class UserEpisode < ActiveRecord::Base
   belongs_to :user
   belongs_to :episode
+  validates_uniqueness_of :episode_id, scope: :user_id
   attr_accessible :user_id, :episode_id
 end
--- spec/models/user_episode_spec.rb
+++ spec/models/user_episode_spec.rb
@@ -4,4 +4,5 @@ describe UserEpisode do
   it { should belong_to(:episode) }
   it { should belong_to(:user) }
   it { should respond_to(:user_id, :episode_id) }
+  it { should validate_uniqueness_of(:episode_id).scoped_to(:user_id) }
 end
