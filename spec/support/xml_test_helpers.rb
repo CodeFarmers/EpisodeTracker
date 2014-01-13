3b86cebf6cb5d51a14164d4c6050b4bd2302a799
Tom Lauwyck <tom.lauwyck@gmail.com>
2014-01-13 18:56:28 +0100

    Removed unused xmlhelper method

--- spec/support/xml_test_helpers.rb
+++ spec/support/xml_test_helpers.rb
@@ -1,10 +1,5 @@
 module XmlTestHelpers
 
-  def get_xml_from_file(path)
-    file = File.open(path)
-    REXML::Document.new file
-  end
-
   def get_text_from_file(path)
     file = File.open(path)
     file.read
