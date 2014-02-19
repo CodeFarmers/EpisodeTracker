fea654961bb7ae37c218c7b3129d1a9e3c198cf8
Tom Lauwyck <tom.lauwyck@gmail.com>
2014-02-19 19:44:46 +0100

    Updating episodes in SeriesUpdater

--- app/interactions/series_updater.rb
+++ app/interactions/series_updater.rb
@@ -3,13 +3,10 @@ class SeriesUpdater
 
   def self.execute
     update_info = REXML::Document.new(ApiConnector.new.retrieve_updates(last_updated_at))
-    puts update_info
-
     UpdateHelper.last_updated_at = update_info.elements["//Time"].text
 
     series_ids = []
     update_info.elements.each("//Series") { |element| series_ids << element.text }
-    ap series_ids
     series_ids.each do |remote_id|
       series = Series.where(remote_id: remote_id).first
       series_update = ApiConnector.new.get_series_update(remote_id)
@@ -18,24 +15,28 @@ class SeriesUpdater
       overview = parsed_series_update.elements["//Overview"].try(:text)
       if series
         series.update_attributes(name: name, overview: overview)
-      else
-        remote_id = parsed_series_update.elements["//id"].try(:text)
-        Series.create(name: name, overview: overview, remote_id: remote_id)
       end
     end
 
+    episode_ids = []
+    update_info.elements.each("//Episode") { |element| episode_ids << element.text }
+    episode_ids.each do |remote_id|
+      episode = Episode.where(remote_id: remote_id).first
+      episode_update = ApiConnector.new.get_episode_update(remote_id)
+      parsed_episode_update = REXML::Document.new(episode_update)
 
+      series_id = parsed_episode_update.elements["//seriesid"].try(:text)
+      name = parsed_episode_update.elements["//EpisodeName"].try(:text)
+      overview = parsed_episode_update.elements["//Overview"].try(:text)
+      season = parsed_episode_update.elements["//SeasonNumber"].try(:text)
+      air_date = parsed_episode_update.elements["//FirstAired"].try(:text)
 
-    episodes = []
-    update_info.elements.each("//Episode") { |element| episodes << element.text }
-    ap episodes
-
-    #series = Series.find_by_remote_id(remote_id)
-    #series_update = ApiConnector.new.get_series_update(remote_id)
-    #parsed_series_update = REXML::Document.new(series_update)
-    #name = parsed_series_update.elements["//SeriesName"].text
-    #overview = parsed_series_update.elements["//Overview"].try(:text)
-    #series.update_attributes(name: name, overview: overview, last_remote_update: last_remote_update(remote_id))
+      if episode
+        episode.update_attributes(name: name, overview: overview, season: season, air_date: air_date)
+      elsif Series.where(remote_id: series_id).any?
+        remote_id = parsed_episode_update.elements["//id"].try(:text)
+        Episode.create!(name: name, overview: overview, series_id: series_id, season: season, air_date: air_date, remote_id: remote_id)
+      end
+    end
   end
-
 end
--- spec/interactions/series_updater_spec.rb
+++ spec/interactions/series_updater_spec.rb
@@ -15,43 +15,80 @@ describe SeriesUpdater do
   describe "execute" do
 
     let!(:series) { FactoryGirl.create(:series, remote_id: 71663) }
+    let(:existing_episode) { FactoryGirl.create(:episode, remote_id: 2, series: series)}
     let(:last_updated_at) { '1362939961' }
 
     before do
-      response = XmlTestHelpers.get_text_from_file("spec/data/remote_id_found.xml")
+      response = XmlTestHelpers.get_text_from_file("spec/data/update_list.xml")
       FakeWeb.register_uri(:get, "http://thetvdb.com/api/Updates.php?type=all&time=#{last_updated_at}", :body => response)
       series_update = XmlTestHelpers.get_text_from_file('spec/data/series_update.xml')
       FakeWeb.register_uri(:get, "http://thetvdb.com/api/4F5EC797A9F93512/series/#{series.remote_id}/en.xml", body: series_update)
+      FakeWeb.register_uri(:get, "http://thetvdb.com/api/4F5EC797A9F93512/series/273979/en.xml", body: "")
+      episode_update = XmlTestHelpers.get_text_from_file('spec/data/episode_update.xml')
+      FakeWeb.register_uri(:get, "http://thetvdb.com/api/4F5EC797A9F93512/episodes/#{existing_episode.remote_id}/en.xml", body: episode_update)
+      FakeWeb.register_uri(:get, "http://thetvdb.com/api/4F5EC797A9F93512/episodes/4/en.xml", body: episode_update)
     end
 
     it "should set the new last updated time" do
-      FakeWeb.register_uri(:get, "http://thetvdb.com/api/4F5EC797A9F93512/series/273979/en.xml", body: "")
-
       SeriesUpdater.execute
       UpdateHelper.last_updated_at.should == '1389028696'
     end
 
     it "should update the series information if it exists" do
-      FakeWeb.register_uri(:get, "http://thetvdb.com/api/4F5EC797A9F93512/series/273979/en.xml", body: "")
-
       SeriesUpdater.execute
       series.reload.name.should == "The Simpsonsupdated"
       series.reload.overview.should == "new overview"
     end
 
-    it "should create the series if it does not yet exist" do
-      unknown_series_update = XmlTestHelpers.get_text_from_file('spec/data/unknown_series_update.xml')
-      FakeWeb.register_uri(:get, "http://thetvdb.com/api/4F5EC797A9F93512/series/273979/en.xml", body: unknown_series_update)
-
-      SeriesUpdater.execute
-
-      unknown_series = Series.where(remote_id: 273979).first
-      unknown_series.name.should == 'Hoeba!'
-      unknown_series.overview.should == 'Snowverview'
+    it "should ignore the series if it does not yet exist" do
+      lambda do
+        SeriesUpdater.execute
+      end.should_not change(Series, :count)
     end
 
-    it "should update the episode information" do
+    context "if the episode exists" do
 
+
+      it "should update the episode information" do
+        SeriesUpdater.execute
+        existing_episode.reload.name.should == "The Ceremony"
+        existing_episode.overview.should == "A sacred ceremony"
+        existing_episode.series_id.should == series.remote_id
+        existing_episode.season.should == 1
+        existing_episode.air_date.should == Date.new(1995, 9, 19)
+      end
+    end
+
+    context "if the episode does not exist" do
+      context "if the series exists" do
+
+        let!(:series_for_episode) { FactoryGirl.create(:series, remote_id: 333)}
+
+        it "should create the episode" do
+          lambda do
+            SeriesUpdater.execute
+          end.should change(Episode, :count).by(1)
+        end
+
+        it "should have created the episode with the correct params" do
+          SeriesUpdater.execute
+          episode = Episode.where(remote_id: 4).first
+          episode.name.should == "The Ceremony"
+          episode.overview.should == "A sacred ceremony"
+          episode.series_id.should == series_for_episode.remote_id
+          episode.season.should == 1
+          episode.air_date.should == Date.new(1995, 9, 19)
+        end
+      end
+
+      context "if the series does not exist" do
+        it "should ignore the episode" do
+          Episode.delete_all
+          lambda do
+            SeriesUpdater.execute
+          end.should_not change(Episode, :count)
+        end
+      end
     end
   end
 end
